740b40b174a92f30074ac66e816ef1e5b240373f
Cameron Grant <32722226+CameronJGrant@users.noreply.github.com>
2024-02-23 13:25:48 -0800

    Support proper naming of pyd file.

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
-
+from pathlib import Path
 from Cython.Build import cythonize
 
 setup(ext_modules=cythonize("PyOMP.pyx", language_level=3))
+
+current_directory = Path(__file__).parent
+pyd_file = list(current_directory.glob('*.pyd'))[0]
+pyd_file.replace(current_directory / f"{pyd_file.stem.split('.')[0]}.pyd")
